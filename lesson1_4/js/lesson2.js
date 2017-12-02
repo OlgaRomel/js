c2d7e984733c8eb4f86b9a5b43f52b3557610945
OlgaRomel <lylik.92@gmail.com>
2017-12-02 12:36:50 +0300

    задача с треугольником

--- lesson1_4/js/lesson2.js
+++ lesson1_4/js/lesson2.js
@@ -3,48 +3,54 @@ console.log("arrays");
 
 
 //dz 1
+console.log("\nЗадача 1");
 var tar = 23;
 var fairy = 34;
-/* while () {
-// 0.5 fairy - 1 tar
-// console.log(fairy)
-} console.log(fairy || tar) */
-var i=0, j=0;
-while (i<tar && j<fairy) {
-    i++;
-    j += 0.5;
+while (tar>0 && fairy>0) {
+    tar--;
+    fairy -= 0.5;
    // console.log(i, j);
 } 
 
-i === tar ? console.log("все тарелки вымыты") : console.log("осталось не намыто ",tar-i ,"тарелок"); 
-j === fairy ? console.log("моющее средство закончилось") : console.log("осталось моющего ",fairy-j ,"средства"); 
+0 === tar ? console.log("все тарелки вымыты") : console.log("осталось не намыто ",tar ,"тарелок"); 
+0 === fairy ? console.log("моющее средство закончилось") : console.log("осталось моющего ",fairy ,"средства"); 
 
 // dz 2
+console.log("\nЗадача 2");
 
 for (var n=1 ; n<=10 ; n++) {
     switch (n) {
         case 1:
-            console.log(n,"фунт =", n*0.454, "кг"); 
+            console.log(n + " фунт =",n*0.454 + " кг"); 
             break;
         case 2: 
         case 3:
         case 4:
-            console.log(n,"фунта =", n*0.454, "кг");
+            console.log(n + " фунта =",n*0.454 + " кг");
             break;
         default:
-            console.log(n,"фунтов =", n*0.454, "кг");
+            console.log(n + " фунтов =",n*0.454 + " кг");
     }
 }
 
 //dz 4
-
+console.log("\nЗадача 4");
 var x1=1, x2=2;
 var y1=3, y2=2;
 var z1=3, z2=5;
-
+console.log("треугольник\nx1 x2", x1,x2, '\ny1 y2', y1,y2, '\nz1 z2', z1,z2);
+var x = x2-x1;
+var y = y2-y1;
+var z = z2-z1;
+if (x===y+z || y===x+z || z===x+y) {
+    console.log("треугольник прямоугольный");
+} else {
+    console.log("треугольник не прямоугольный")
+}
 // dz 5
+console.log("\nЗадача 5");
 var arrNum = [21, 14, 6, 8, 19, 3];
-console.log(arrNum);
+console.log("массив\n",arrNum);
 
 for (let a = arrNum.length; a>0; a--) {
     for (let b = 0; b<a; b++) {
