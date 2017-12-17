d43fe109ada2fa50f8716c07d881a7099398b1bc
OlgaRomel <lylik.92@gmail.com>
2017-12-17 20:22:04 +0300

    dz

--- lesson1_4/js/lesson4.js
+++ lesson1_4/js/lesson4.js
@@ -2,48 +2,78 @@
 'use strict';
 console.log("1 задача");
 
-var  sity1 = {};
-sity1.name = "Soul";
-sity1.population = "400000";
-sity1.mayor = "Hyun";
+var  city1 = {};
+city1.name = "Soul";
+city1.population = "400000";
+city1.mayor = "Hyun";
 
-var sity2 = {
+var city2 = {
     name: "SaintPetersburg",
     population: "500000",
     mayor: "Georg",
 };
 
-console.log(sity1);
-console.log(sity2);
+console.log(city1);
+console.log(city2);
 
-sity1.seizure = function(n) {
-    sity1.population = parseInt(sity1.population) + n;
-    sity2.population = parseInt(sity2.population) - n;
+city1.seizure = function(n) {
+    this.population = parseInt(this.population) + n;
+    city2.population = parseInt(city2.population) - n;
 }
 
-sity1.seizure(1000);
-console.log(sity1.population, sity2.population);
+city1.seizure(1000);
+console.log(city1.population, city2.population);
 
 var mayorName = function(name) {
-    if (sity1.mayor.localeCompare(name) === 0) {
-        console.log ("mayor of sity ",sity1.name);
+    if (city1.mayor.localeCompare(name) === 0) {
+        console.log ("mayor of sity ",city1.name);
     }else {
-        console.log ("mayor of sity ",sity2.name);
+        console.log ("mayor of sity ",city2.name);
     }
 };
 
+function showMayor () {
+    console.log(this.mayor);
+}
+
+function changeMayor(new_val) {
+    this.mayor = new_val;
+    console.log(this.mayor);
+}
+
+showMayor.call(city1);//["New Name"]
+showMayor.apply(city1);//[arr]
+
+
 mayorName("Georg");
 
+
+var president = {
+    changeCountryMayor: function (new_name) {
+        this.mayor = new_name;
+    },
+ //   getName: function() {
+ //       return this.name;
+ //   }
+};
+
+president.changeCountryMayor.call(city1, "m name");
+console.log(president, city1);
+console.log(president, city1.mayor);
+
+
+
+/*
 var president = {
     changeCountryMayor: function (argument) {
-        if (argument === sity1.name) {
-            president = sity1;
+        if (argument === city1.name) {
+            president = city1;
             president.mayor = "Tolya"; 
-            console.log("New mayor ",sity1.name, sity1.mayor);        
+            console.log("New mayor ",city1.name, city1.mayor);        
         }else {
-            president = sity2;
+            president = city2;
             president.mayor = "Slava";
-            console.log("New mayor ",sity2.name, sity2.mayor);  
+            console.log("New mayor ",city2.name, city2.mayor);  
         }
     },
 };
@@ -60,10 +90,28 @@ var test = function(logos) {
     }
 };
 
-test();
+//test();
+*/
+console.log("2 задача");
 
+var str = "fanfarionfar";
+var str1 = 'far';
+// console.log (str);
+// var substr = str.substring(3,6);
+// console.log (substr);
 
+function search(str, substr) {
+    var i = 0;
+    var j = 0;
+    while (true) {
+        var len = str.indexOf(str1,i);
+        if (len == -1) break;
+        i = len + 1;
+        j++;
+    } return j;
+};
 
+console.log("количество повторений подстроки", search());
 
 
 }())
